978d31a1c96aaf4ff77f6b4de06953a13df8017d
aiyu666 <zrobin8741@gmail.com>
2021-10-02 23:03:37 +0800

    Update sum.cpp

--- twoValueSum/sum.cpp
+++ twoValueSum/sum.cpp
@@ -3,7 +3,7 @@ int main()
 {
     std::cout << "Hello world 1 " << std::endl;
     int v1 = 0, v2 = 0;
-    std::cout << "Input two number:";
+    std::cout << "Input two number:" << std::endl;
     std::cin >> v1 >> v2;
     std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
     return 0;
