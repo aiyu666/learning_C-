dca265dde6f25b22ac9d5e137e13a1b588ba004c
aiyu666 <zrobin8741@gmail.com>
2021-10-02 23:27:11 +0800

    practice common out

--- practice/1.2/validCode_1.6.cpp
+++ practice/1.2/validCode_1.6.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+
+/*
+實作一個簡單的加法和練習註解 
+*/
 int main()
 {
-    int v1 = 2, v2 = 4;
+    int v1 = 2, v2 = 4; // 宣告 v1 和 v2 變數
     std::cout << "The sum of " << v1
               << " and " << v2
               << " is " << v1 + v2 << std::endl;
