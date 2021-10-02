661bc471484ef0915f4d6417828c42e9bfe270ce
aiyu666 <zrobin8741@gmail.com>
2021-10-02 23:24:52 +0800

    Complete practice 1.2

---
+++ practice/1.2/twoNumberMultiply_1.4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Please input two number: " << std::endl;
+    int v1, v2;
+    std::cin >> v1 >> v2;
+    std::cout << "The v1 is " << v1 << ", V2 is " << v2 << " and the sum is " << v1 * v2 << std::endl;
+}
---
+++ practice/1.2/validCode_1.6.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    int v1 = 2, v2 = 4;
+    std::cout << "The sum of " << v1
+              << " and " << v2
+              << " is " << v1 + v2 << std::endl;
+}
